a02c6822fd449bb644150715eee5fdaf52a2b61b
Pavel <work.belyavsky.p.a@gmail.com>
2024-02-15 15:18:35 +0300

    fix: import name for @NotNull annotation

--- generators/imports.py
+++ generators/imports.py
@@ -2,6 +2,6 @@ from enum import Enum
 
 class Imports(Enum):
     SerializedName = "import com.google.gson.annotations.SerializedName;"
-    NotNull = "import jakarta.validation.contraints.NotNull;"
+    NotNull = "import jakarta.validation.constraints.NotNull;"
     Objects = "import java.util.Objects;"
     List = "import java.util.List;"
