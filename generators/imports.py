47059ed8a87788e71b23f2a21be01875576d233b
Pavel <work.belyavsky.p.a@gmail.com>
2024-05-17 23:26:26 +0300

    feat: updated import for NotNull annotation

--- generators/imports.py
+++ generators/imports.py
@@ -2,11 +2,11 @@ from enum import Enum
 
 class Imports(Enum):
     SerializedName = "import com.google.gson.annotations.SerializedName;"
-    NotNull = "import jakarta.validation.constraints.NotNull;"
     Objects = "import java.util.Objects;"
     List = "import java.util.List;"
     MessageOrBoolean = "import jarkz.tbot.types.MessageOrBoolean;"
     Id = "import jarkz.tbot.types.Id;"
+    NotNull = "import jarkz.tbot.types.annotations.NotNull;"
     InputFile = "import jarkz.tbot.types.InputFile;"
 
     def as_line(self) -> str:
